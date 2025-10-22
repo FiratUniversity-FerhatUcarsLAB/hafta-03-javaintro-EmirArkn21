a37b91eb4304e6f18076d73ad078e08748ba315c
EmirArkn21 <250541005@firat.edu.tr>
2025-10-23 00:15:56 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,15 @@
+public class DikdortgenHesap {
+    public static void main(String[] args) {
+        double en = 4.5;
+        double boy = 7.9;
+        // en ve boy tanımladık.
+        double alan = en * boy;
+        // alan cevre hesaplamasını yaptırdık.
+        double cevre = (en+boy)*2;
+        // ekrana yazdırdık.
+        System.out.println("En:"+ en);
+        System.out.println("Boy:"+ boy);
+        System.out.println("Alan:"+ alan);
+        System.out.println("Çevre:"+ cevre);
+    }
+}
