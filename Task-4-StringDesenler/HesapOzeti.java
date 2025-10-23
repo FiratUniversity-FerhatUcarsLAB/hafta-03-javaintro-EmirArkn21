e91254ff0765d49f658a5f38d02203d3cb5f14f4
EmirArkn21 <250541005@firat.edu.tr>
2025-10-23 17:05:04 +0300

    Update HesapOzeti.java

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,12 @@
+public class HesapOzeti {
+    public static void main(String[] args) {
+        System.out.println("------ Alışveriş Özeti ------");
+        System.out.println("Ürün      | Miktar | Fiyat");
+        System.out.println("-----------------------------");
+        System.out.println("Elma      |   3    | 15.0");
+        System.out.println("Ekmek     |   2    | 12.5");
+        System.out.println("Süt       |   1    | 17.9");
+        System.out.println("-----------------------------");
+        System.out.println("Toplam              45.4");
+    }
+}
