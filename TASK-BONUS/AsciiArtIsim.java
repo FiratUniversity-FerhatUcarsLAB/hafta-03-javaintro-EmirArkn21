360e433d11d3ffc1d6447a4a5f9a23996011a806
EmirArkn21 <250541005@firat.edu.tr>
2025-10-23 18:11:16 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,11 @@
+public class AsciiArtIsim {
+    public static void main(String[] args) {
+
+        System.out.println("EEEEE  M   M  IIIII  RRRR     ");
+        System.out.println("E      MM MM    I    R   R    ");
+        System.out.println("EEE    M M M    I    RRRR     ");
+        System.out.println("E      M   M    I    R R      ");
+        System.out.println("EEEEE  M   M  IIIII  R  R     ");
+
+    }
+}
