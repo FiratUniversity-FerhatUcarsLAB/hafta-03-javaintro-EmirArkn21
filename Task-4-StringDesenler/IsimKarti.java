0b5a107dc4ca08ca1809c2fd752be34e27f6a0f2
EmirArkn21 <250541005@firat.edu.tr>
2025-10-23 17:05:21 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,18 @@
+public class IsimKarti {
+    public static void main(String[] args) {
+         String ad = " Emirhan ";
+         String soyad = " ARIKAN ";
+         String ogrno = " 250541005 ";
+         String bolum = " Yazilim Muhendisligi";
+        //stringle nesneyi belirtiyoruz println de yazdırma yerinde kullanıyoruz bunu.
+
+         System.out.println (" =================== ") ;
+         System.out.println (" OGRENCI BILGILERI ") ;
+         System.out.println (" =================== ") ;
+         System.out.println (" Ad : " + ad ) ;
+         System.out.println (" Soyad : " + soyad ) ;
+         System.out.println (" No : " + ogrno ) ;
+         System.out.println (" Bolum : " + bolum ) ;
+         System.out.println (" =================== ");
+    }
+}
